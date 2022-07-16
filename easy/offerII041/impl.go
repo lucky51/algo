f8866f5c297e24752d9d73a128112fd47779a090
lucky51 <gopher666@163.com>
2022-07-16 20:30:49 +0800

    add daily task

---
+++ easy/offerII041/impl.go
@@ -0,0 +1,20 @@
+package offerii041
+
+// 滑动窗口的平均值 TODO: 2022-07-16每日一题未实现
+type MovingAverage struct {
+}
+
+/** Initialize your data structure here. */
+func Constructor(size int) MovingAverage {
+
+}
+
+func (this *MovingAverage) Next(val int) float64 {
+
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * obj := Constructor(size);
+ * param_1 := obj.Next(val);
+ */
