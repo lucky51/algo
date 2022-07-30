970794eefeeca4af29081833b33e1532917c5639
lucky51 <gopher666@163.com>
2022-07-30 09:48:59 +0800

    add daily task

---
+++ hard/leetcode952/impl.go
@@ -0,0 +1,6 @@
+package leetcode952
+
+// largestComponentSize 按公因数计算最大组件大小
+func largestComponentSize(nums []int) int {
+
+}
---
+++ medium/leetcode593/impl.go
@@ -0,0 +1,6 @@
+package leetcode593
+
+// validSquare 有效的正方形
+func validSquare(p1 []int, p2 []int, p3 []int, p4 []int) bool {
+
+}
