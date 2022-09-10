8e6395c54a7ab31d5016e631b8e2684176ada956
lucky51 <gopher666@163.com>
2022-09-10 21:22:09 +0800

    add trimBST comment

--- medium/leetcode669/impl.go
+++ medium/leetcode669/impl.go
@@ -7,9 +7,11 @@ func trimBST(root *TreeNode, low int, high int) *TreeNode {
 	if root == nil {
 		return nil
 	}
+	// 如果根节点的值小于最小值，则根节点和左子节点的值都不符合要求，就判断右子节点
 	if root.Val < low {
 		return trimBST(root.Right, low, high)
 	}
+	// 如果根节点的值大于最大值，则跟节点和右节点的值都不符合要求，就判断右子节点
 	if root.Val > high {
 		return trimBST(root.Left, low, high)
 	}
