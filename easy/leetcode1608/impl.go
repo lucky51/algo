a7a96e3489d15b3eb47c7d598f976dd8f4d78cab
lucky51 <gopher666@163.com>
2022-09-12 15:28:03 +0800

    add daily algo

---
+++ easy/leetcode1608/impl.go
@@ -0,0 +1,16 @@
+package leetcode1608
+
+import "sort"
+
+// specialArray 特殊数组的特征值
+func specialArray(nums []int) int {
+	n := len(nums)
+	sort.Ints(sort.Reverse(sort.IntSlice(nums)))
+	for i := 1; i <= n; i++ {
+		// 这里说的是恰好，而不是大于等于
+		if nums[i-1] > i && (i == n || nums[i] < i) {
+			return i
+		}
+	}
+	return -1
+}
