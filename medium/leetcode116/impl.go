3d0c94f06da6711ac55bff1fa9fe6d94e49535a7
lucky51 <gopher666@163.com>
2022-10-14 16:54:47 +0800

    add prefect tree connect

---
+++ medium/leetcode116/impl.go
@@ -0,0 +1,33 @@
+package leetcode116
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+// connect 填充每个节点的下一个右侧节点指针
+func connect(root *Node) *Node {
+	if root == nil {
+		return nil
+	}
+	q := []*Node{root}
+	for len(q) > 0 {
+		tmp := q
+		q = nil
+
+		for i := 0; i < len(tmp); i++ {
+			if i+1 < len(tmp) {
+				tmp[i].Next = tmp[i+1]
+			}
+			if tmp[i].Left != nil {
+				q = append(q, tmp[i].Left)
+			}
+			if tmp[i].Right != nil {
+				q = append(q, tmp[i].Right)
+			}
+		}
+	}
+	return root
+}
