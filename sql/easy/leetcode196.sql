4241be0ab61de0b7c65643af98ffca4c294970f1
lucky51 <gopher666@163.com>
2022-10-16 12:11:57 +0800

    add sql delete duplicate emails

---
+++ sql/easy/leetcode196.sql
@@ -0,0 +1,7 @@
+-- 删除重复的电子邮箱
+
+delete from Person  where id in (
+  select id from ( select   p.id,ROW_NUMBER() OVER(PARTITION BY p.email order by p.id ) rn from Person p  ) as tb where tb.rn !=1
+
+)
+
