d4d3c29391612c8d266f2ede469bedc16e5fe5a0
lucky51 <gopher666@163.com>
2022-10-18 15:34:00 +0800

    update at most n given digitset

--- hard/leetcode902/impl.go
+++ hard/leetcode902/impl.go
@@ -10,7 +10,7 @@ import "strconv"
 来源：力扣（LeetCode）
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 */
-
+// 902. 最大为 N 的数字组合
 func atMostNGivenDigitSet(digits []string, n int) int {
 	m := len(digits)
 	s := strconv.Itoa(n)
@@ -22,13 +22,15 @@ func atMostNGivenDigitSet(digits []string, n int) int {
 			if d[0] == s[i-1] {
 				dp[i][1] = dp[i-1][1]
 			} else if d[0] < s[i-1] {
+				// dp[i][0]=m+dp[i−1][0]×m+dp[i−1][1]×C[i]
+				// C[i] 表示数组digits中小于n的第i位数字的元素个数 ,这里的 s[i-1] 就是 n[i]，
+				// 这个循环分支就是在数 c[i]的个数,循环了几次就是加了几次 相当于 ×C[i]
 				dp[i][0] += dp[i-1][1]
 			} else {
 				break
 			}
 		}
 		if i > 1 {
-			// 第i-1位小于n的数有  dp[i-1] *m种可能 dp[i] 可选的数字就有 m种 累加到 当前 dp[i]
 			dp[i][0] += m + dp[i-1][0]*m
 		}
 	}
@@ -36,6 +38,10 @@ func atMostNGivenDigitSet(digits []string, n int) int {
 }
 
 /*
+
+C[i] 表示数组digits中小于n的第i位数字的元素个数
+
+
  假如第一位严格小于n的第一位那么后面的几位不受限制了，相当于求得k-1位 所以有 k-1 * size种可能  ，如果
  https://leetcode.cn/problems/numbers-at-most-n-given-digit-set/solutions/1899943/shu-wei-dp-by-heren1229-92xc/
 */
