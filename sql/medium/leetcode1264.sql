46225f60ba8fee433cafa2a1136db8f9acfeb8b0
lucky51 <gopher666@163.com>
2022-10-27 10:19:26 +0800

    add sql find the team size

--- sql/medium/leetcode1264.sql
+++ sql/medium/leetcode1264.sql
@@ -1,11 +1,23 @@
 -- 页面推荐
 
-
-
-select * from (
-    select  DISTINCT  if(f.user1_id=1,f.user2_id,f.user1_id)  friendid
-from Friendship f 
-) as tb  join  Likes li on tb.friendid = li.user_id
-where  li.page_id not IN (
- select DISTINCT  page_id from Likes where user_id =1
-) 
+select
+    DISTINCT li.page_id as recommended_page
+from (
+        select
+            DISTINCT if(
+                f.user1_id = 1,
+                f.user2_id,
+                f.user1_id
+            ) friendid
+        from Friendship f
+        where
+            f.user1_id = 1
+            or f.user2_id = 1
+    ) as tb
+    join Likes li on tb.friendid = li.user_id
+where li.page_id not IN (
+        select
+            DISTINCT page_id
+        from Likes
+        where user_id = 1
+    )
