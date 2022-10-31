01d1ae5cee7db9d4b8bac6998a37587cd1c9be36
lucky51 <gopher666@163.com>
2022-10-31 11:33:51 +0800

    add sql product price at a given date

---
+++ sql/medium/leetcode1164.sql
@@ -0,0 +1,18 @@
+-- 指定日期的产品价格
+
+select p1.product_id, ifnull(p2.new_price, 10) as price
+from (
+    select distinct product_id
+    from products
+) as p1 -- 所有的产品
+left join (
+    select product_id, new_price 
+    from products
+    where (product_id, change_date) in (
+        select product_id, max(change_date)
+        from products
+        where change_date <= '2019-08-16'
+        group by product_id
+    )
+) as p2 -- 在 2019-08-16 之前有过修改的产品和最新的价格
+on p1.product_id = p2.product_id
