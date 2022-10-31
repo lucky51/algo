7f151def4fa4ea5c4d4da7c984f0e15b4b49c79d
lucky51 <gopher666@163.com>
2022-10-31 10:36:12 +0800

    add sql exchange seats

---
+++ sql/medium/leetcode626.sql
@@ -0,0 +1,8 @@
+-- 换座位
+
+# Write your MySQL query statement below
+select
+    s1.id,
+    ifnull(s2.student, s1.student) as student
+from Seat s1
+    LEFT join Seat s2 on s1.id + if(s1.id % 2 = 1, 1, -1) = s2.id
