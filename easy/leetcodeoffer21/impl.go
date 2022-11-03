89221608ab64a89f6482e55a780e323364998bfd
lucky51 <gopher666@163.com>
2022-11-03 14:31:35 +0800

    add offer exchange by double pointers

--- easy/leetcodeoffer21/impl.go
+++ easy/leetcodeoffer21/impl.go
@@ -12,3 +12,16 @@ func exchange(nums []int) []int {
 	})
 	return nums
 }
+
+// exchange1 双指针解决奇偶调换
+func exchange1(nums []int) []int {
+	left, right := 0, len(nums)-1
+	for left < right {
+		for ; left < right && nums[left]%2 == 1; left++ {
+		}
+		for ; left < right && nums[right]%2 == 0; right-- {
+		}
+		nums[left], nums[right] = nums[right], nums[left]
+	}
+	return nums
+}
