984f6ac4e8ef661d3d9a24ea3f27da09d892ef64
lucky51 <gopher666@163.com>
2022-11-04 11:42:50 +0800

    add min number

--- sql/medium/leetcode1596.sql
+++ sql/medium/leetcode1596.sql
@@ -1,7 +1,10 @@
 -- 每位顾客最经常订购的商品
 
-SELECT Orders.customer_id  FROM (
-    select customer_id ,product_id ,count(product_id) as cnt FROM Orders GROUP BY customer_id ,product_id
-) as t
-JOIN Customers ON t.customer_id=Customers.customer_id
-JOIN Products on t.product_id = Products.product_id
+
+SELECT o.customer_id,o.product_id,p.product_name from products p,
+
+(
+    SELECT customer_id,product_id,rank() over(partition by customer_id order by count(1) desc) rk
+FROM orders GROUP BY customer_id,product_id
+) as o
+where o.rk =1 and p.product_id = o.product_id
