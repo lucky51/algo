990c229b913caf9e582cead9b0feb023da1514f0
lucky51 <gopher666@163.com>
2022-11-11 13:57:25 +0800

    add format

--- sql/hard/leetcode1767.sql
+++ sql/hard/leetcode1767.sql
@@ -1,11 +1,25 @@
 -- 寻找没有被执行的任务对
 
-
 with recursive a as (
-    SELECT task_id,1 as subtask_id,subtasks_count FROM Tasks
-    UNION ALL
-    SELECT t.task_id,t.subtask_id+1 as c,t.subtasks_count FROM  a t WHERE  subtask_id <t.subtasks_count
-)
-select * from  Executed WHERE (task_id ,subtask_id) not IN (
-    SELECT task_id,subtask_id FROM a
-)
+        SELECT
+            task_id,
+            1 as subtask_id,
+            subtasks_count
+        FROM Tasks
+        UNION ALL
+        SELECT
+            t.task_id,
+            t.subtask_id + 1 as c,
+            t.subtasks_count
+        FROM a t
+        WHERE
+            subtask_id < t.subtasks_count
+    )
+select task_id, subtask_id
+from a
+WHERE (task_id, subtask_id) not IN (
+        SELECT
+            task_id,
+            subtask_id
+        FROM Executed
+    )
