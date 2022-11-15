54ae7dee8e12cbe3ab08767b8fd9508167575226
lucky51 <gopher666@163.com>
2022-11-15 10:23:23 +0800

    upgrade reverse words

--- easy/leetcodeoffer58I/impl.go
+++ easy/leetcodeoffer58I/impl.go
@@ -1,13 +1,18 @@
 package leetcodeoffer58i
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+)
 
 // reverseWords 剑指Offer I 翻转单词顺序
+// 使用库函数求解，不使用的话函数很长，每一个功能都要手动写
 func reverseWords(s string) string {
-	strs := strings.Split(s, " ")
+	s = strings.Trim(s, " ")
+	strs := regexp.MustCompile(`\s+`).Split(s, -1)
 	n := len(strs)
 	for i := 0; i < n/2; i++ {
 		strs[i], strs[n-i-1] = strs[n-i-1], strs[i]
 	}
-	return strings.Trim(strings.Join(strs, " "), " ")
+	return strings.Join(strs, " ")
 }
