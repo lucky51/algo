a81a3ac385f1dd1d5e5eab78b595892ba0ffa098
lucky51 <gopher666@163.com>
2022-11-16 14:04:58 +0800

    add min stack

---
+++ easy/leetcodeoffer30/impl.go
@@ -0,0 +1,54 @@
+package leetcodeoffer30
+
+import "math"
+
+type MinStack struct {
+	stack    []int
+	minStack []int
+}
+
+/** initialize your data structure here. */
+func Constructor() MinStack {
+	return MinStack{stack: []int{}, minStack: []int{math.MaxInt64}}
+}
+
+func (this *MinStack) Push(x int) {
+	this.stack = append(this.stack, x)
+	top := this.minStack[len(this.minStack)-1]
+	this.minStack = append(this.minStack, min(top, x))
+}
+
+func (this *MinStack) Pop() {
+	if len(this.stack) > 0 {
+		this.stack = this.stack[:len(this.stack)-1]
+		this.minStack = this.minStack[:len(this.minStack)-1]
+	}
+
+}
+
+func (this *MinStack) Top() int {
+	if len(this.stack) > 0 {
+		return this.stack[len(this.stack)-1]
+	}
+	return 0
+}
+
+func (this *MinStack) Min() int {
+	return this.minStack[len(this.minStack)-1]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.Min();
+ */
